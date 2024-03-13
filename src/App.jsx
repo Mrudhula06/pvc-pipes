96d4bf881a0ed7b64e0030927d1be1b6998f0118
MRUDHULA SHRI M <mrudhulashrimanikantan@gmail.com>
2024-03-13 18:09:18 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./Components/Home";
 import Nav from "./Components/Nav";
+import About from "./Components/About";
 
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
+    <div className='overflow-hidden'>
       <Nav />
       <Home />
-    </Router>
+      <About />
+    </div>
   );
 }
 
