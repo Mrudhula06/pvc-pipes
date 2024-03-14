dfc44e515c8ce81258f5d9f1bc9393dc9b6c79f7
MRUDHULA SHRI M <mrudhulashrimanikantan@gmail.com>
2024-03-14 19:06:32 +0530

    home content

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+  import React from 'react'
 
-function Home() {
-  return (
-    <div className="absolute">
-      home is where the heart is
-      <h1>home is where the heart is</h1>
-      <h1>home is where the heart is</h1>
-      <h1>home is where the heart is</h1>
-      <h1>home is where the heart is</h1>
-    </div>
-  )
-}
+  function Home() {
+    return (
+      <div className="absolute" style={{ paddingTop: '80px' }}>
+        <div className="p-8">
+        <h1>home is where the heart is</h1>
+        <h1>home is where the heart is</h1>
+        <h1>home is where the heart is</h1>
+        <h1>home is where the heart is</h1>
+      </div>
+      </div>
+    )
+  }
 
-export default Home
+  export default Home
